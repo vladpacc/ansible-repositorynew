857da9f61b2fa66034e5ee39ef0465329e62aaec
EC2 Default User <ec2-user@ip-172-31-30-252.eu-west-2.compute.internal>
2020-04-16 02:02:17 +0000

    changed playbook

--- dynamic/ec2.py
+++ dynamic/ec2.py
@@ -3,42 +3,29 @@
 '''
 EC2 external inventory script
 =================================
-
 Generates inventory that Ansible can understand by making API request to
 AWS EC2 using the Boto library.
-
 NOTE: This script assumes Ansible is being executed where the environment
 variables needed for Boto have already been set:
     export AWS_ACCESS_KEY_ID='AK123'
     export AWS_SECRET_ACCESS_KEY='abc123'
-
 Optional region environment variable if region is 'auto'
-
 This script also assumes that there is an ec2.ini file alongside it.  To specify a
 different path to ec2.ini, define the EC2_INI_PATH environment variable:
-
     export EC2_INI_PATH=/path/to/my_ec2.ini
-
 If you're using eucalyptus you need to set the above variables and
 you need to define:
-
     export EC2_URL=http://hostname_of_your_cc:port/services/Eucalyptus
-
 If you're using boto profiles (requires boto>=2.24.0) you can choose a profile
 using the --boto-profile command line argument (e.g. ec2.py --boto-profile prod) or using
 the AWS_PROFILE variable:
-
     AWS_PROFILE=prod ansible-playbook -i ec2.py myplaybook.yml
-
 For more details, see: http://docs.pythonboto.org/en/latest/boto_config_tut.html
-
 You can filter for specific EC2 instances by creating an environment variable
 named EC2_INSTANCE_FILTERS, which has the same format as the instance_filters
 entry documented in ec2.ini.  For example, to find all hosts whose name begins
 with 'webserver', one might use:
-
     export EC2_INSTANCE_FILTERS='tag:Name=webserver*'
-
 When run against a specific host, this script returns the following variables:
  - ec2_ami_launch_index
  - ec2_architecture
@@ -96,25 +83,20 @@ When run against a specific host, this script returns the following variables:
  - ec2_tenancy
  - ec2_virtualization_type
  - ec2_vpc_id
-
 These variables are pulled out of a boto.ec2.instance object. There is a lack of
 consistency with variable spellings (camelCase and underscores) since this
 just loops through all variables the object exposes. It is preferred to use the
 ones with underscores when multiple exist.
-
 In addition, if an instance has AWS tags associated with it, each tag is a new
 variable named:
  - ec2_tag_[Key] = [Value]
-
 Security groups are comma-separated in 'ec2_security_group_ids' and
 'ec2_security_group_names'.
-
 When destination_format and destination_format_tags are specified
 the destination_format can be built from the instance tags and attributes.
 The behavior will first check the user defined tags, then proceed to
 check instance attributes, and finally if neither are found 'nil' will
 be used instead.
-
 'my_instance': {
     'region': 'us-east-1',             # attribute
     'availability_zone': 'us-east-1a', # attribute
@@ -124,13 +106,11 @@ be used instead.
     'ec2_tag_Name': 'webserver',       # tag
     ...
 }
-
 Inside of the ec2.ini file the following settings are specified:
 ...
 destination_format: {0}-{1}-{2}-{3}
 destination_format_tags: Name,clusterid,deployment,private_dns_name
 ...
-
 These settings would produce a destination_format as the following:
 'webserver-ansible-blue-172.31.0.1'
 '''
